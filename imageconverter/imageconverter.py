5f0eee77ba754b50c30aec7b8604881f9a2b2f74
JohnLuke Thermos <jlthermos@gmail.com>
2024-03-09 16:52:54 -0500

    image converter
    Image converter finished, only works with jpg and png at the moment

--- imageconverter/imageconverter.py
+++ imageconverter/imageconverter.py
@@ -1,16 +1,21 @@
 from PIL import Image
-
+import os
 def main():
-    imagefile = input("Enter the name of the image file you want to convert: ")
-    if imagefile[-1] ==  'g':
+    imagefile = input("Enter the path of the image (PNG or JPG) file you want to convert: ").strip('"')
+    #PNG detected
+    if imagefile[-2] ==  'n':
         input("PNG detected, press enter to convert to JPG...")
         img = Image.open(imagefile)
-        img.save(imagefile.replace("png", "jpg"))
+        rgb_im = img.convert('RGB')
+        rgb_im.save(imagefile.replace("png", "jpg"))
+        os.remove(imagefile) #remove the original PNG file
         print("Image converted to JPG")
-    elif imagefile[-1] == 'g':
+    #JPG detected
+    elif imagefile[-2] == 'p':
         input("JPG detected, press enter to convert to PNG...")
         img = Image.open(imagefile)
         img.save(imagefile.replace("jpg", "png"))
+        os.remove(imagefile) #remove the original JPG file
         print("Image converted to PNG")    
     else:
         print("Image file must be either a PNG or JPG")
@@ -20,7 +25,7 @@ main()
 
 
 
-
+#loop
 if __name__ == "__main__":
     main()
 
